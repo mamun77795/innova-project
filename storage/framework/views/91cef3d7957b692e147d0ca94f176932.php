f6fe28bdd07918c47ab0e547076ecb567ad3199b
Mizanur Rahman <37808518+mamun77795@users.noreply.github.com>
2023-05-17 05:42:57 +0600

    Add files via upload

---
+++ storage/framework/views/91cef3d7957b692e147d0ca94f176932.php
@@ -0,0 +1,34 @@
+
+
+
+<?php $__env->startSection('page'); ?>
+<a class='btn btn-success' href="<?php echo e(route('users.index')); ?>">Manage</a>
+<form action="<?php echo e(route('users.store')); ?>" method ="post" enctype="multipart/form-data">
+<?php echo csrf_field(); ?>
+<div class="row mb-3">
+	<label for="username" class="col-sm-2 col-form-label">Username</label>
+	<div class="col-sm-10">
+		<input type = "text" class="form-control" name="username" id="username" placeholder="Username">
+	</div>
+</div>
+<div class="row mb-3">
+	<label for="email" class="col-sm-2 col-form-label">Email</label>
+	<div class="col-sm-10">
+		<input type = "text" class="form-control" name="email" id="email" placeholder="Email">
+	</div>
+</div>
+<div class="row mb-3">
+	<label for="password" class="col-sm-2 col-form-label">Password</label>
+	<div class="col-sm-10">
+		<input type = "text" class="form-control" name="password" id="password" placeholder="Password">
+	</div>
+</div>
+
+<button type="submit" class="btn btn-primary">Save</button>
+</form>
+
+
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout.erp.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\innova-project\resources\views/pages/erp/user/create.blade.php ENDPATH**/ ?>
